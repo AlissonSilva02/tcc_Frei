942daebe415f5d877c0f4b04b3b6c300e89c0b97
alisonfudido <alissonfariassilva02@gmail.com>
2024-09-13 15:25:59 -0300

    Subindo projeto

---
+++ src/App/app.jsx
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+export default App;
+
+function App() {
+  return (
+
+    <div className='secao-landing'>
+
+<div>
+<img src="public\assets\images\menu.png" alt="" />
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
+  );
+}
+
+
---
+++ src/components/head.jsx
@@ -0,0 +1,10 @@
+import 'head.scss'
+
+
+export default function Cabe() {
+
+
+
+    <img src="public\assets\images\menu.png" alt="" />
+    
+}
