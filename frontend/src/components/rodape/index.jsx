4f516c85bcce6be5a58b44e074c75e8a063d788c
brunoyukio3 <ra24224603802@acaonsfatima.org.br>
2024-09-24 17:56:30 -0300

    Rodape

--- frontend/src/components/rodape/index.jsx
+++ frontend/src/components/rodape/index.jsx
@@ -1,32 +1,61 @@
-import "./index.scss";
+import './index.scss'
+
 
 export default function Rodape() {
-    return (
-        <div className="secao-rodape">
-            <div className="cor">
-                <div className="sla">
-                    <hr />
-                </div>
+    return(
 
-                <div className="logo2">
-                    <img
-                        className="logop"
-                        src="assets\images\logo1.png"
-                        alt=""
-                    />
-                </div>
-                <div className="grupo">
-                    <img src="assets\images\I.png" alt="" width={14} />
-                    <img
-                        src="assets\images\Ic.png"
-                        alt=""
-                        width={18}
-                        className="yt"
-                    />
-                    <img src="assets\images\Ico.png" alt="" width={14} />
-                    <img src="assets\images\Icon.png" alt="" width={14} />
-                </div>
-            </div>
+    
+    <div className='secao-rodape'>
+
+        <div className='a1'>
         </div>
-    );
+
+        <div className='b'>
+
+        <div className='b1'></div>
+      
+        <div className='sla'>
+            <img className='asd' src="assets/imgaes/linha.png" alt="" />
+        </div>
+        
+        <div className='b2'></div>
+        </div>
+        
+        
+        <div className='cor'>
+
+        
+
+                <div className='logo2'>
+                <img className='logop'src="assets\images\logo1.png" alt="" />
+                </div>
+            <div className='grupo'>
+
+             <div> 
+            <img src="assets\images\I.png" alt=""  width={14}/>
+            </div>  
+
+
+            <div>
+            <img src="assets\images\Ic.png" alt="" className='yt'/>
+            </div>
+
+            <div>
+            <img src="assets\images\Ico.png" alt="" width={14} />
+            </div>
+
+            <div>
+            <img src="assets\images\Icon.png" alt="" width={14} />
+            </div>
+
+            </div>
+
+
+        </div>
+
+
+
+
+    </div>
+    )
 }
