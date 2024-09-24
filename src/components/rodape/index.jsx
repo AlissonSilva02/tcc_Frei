523e122422bc03fbb1696162cb9f3321bccaf3e9
brunoyukio3 <ra24224603802@acaonsfatima.org.br>
2024-09-24 15:26:22 -0300

    zasgf

--- src/components/rodape/index.jsx
+++ src/components/rodape/index.jsx
@@ -2,25 +2,27 @@ import './index.scss'
 
 
 export default function Rodape() {
+    return(
 
+    
     <div className='secao-rodape'>
 
         <div className='cor'>
 
-        <div>
+        <div className='sla'>
                 <hr />
 
         </div>
 
-                <div>
-                <img src="assets\images\logo1.png" alt="" />
+                <div className='logo2'>
+                <img className='logop'src="assets\images\logo1.png" alt="" />
                 </div>
-
-            <img src="assets\images\I.png" alt="" />
-            <img src="assets\images\Ic.png" alt="" />
-            <img src="assets\images\Ico.png" alt="" />
-            <img src="assets\images\Icon.png" alt="" />
-
+            <div className='grupo'>
+            <img src="assets\images\I.png" alt=""  width={14}/>
+            <img src="assets\images\Ic.png" alt="" width={18} className='yt'/>
+            <img src="assets\images\Ico.png" alt="" width={14} />
+            <img src="assets\images\Icon.png" alt="" width={14} />
+            </div>
 
 
         </div>
@@ -29,5 +31,5 @@ export default function Rodape() {
 
 
     </div>
-    
+    )
 }
