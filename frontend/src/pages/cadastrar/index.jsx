59a50b5abc01a3e550acbaa76c6408b1107ce466
brunoyukio3 <ra24224603802@acaonsfatima.org.br>
2024-10-05 13:20:54 -0300

    consultar em progresso

--- frontend/src/pages/cadastrar/index.jsx
+++ frontend/src/pages/cadastrar/index.jsx
@@ -54,6 +54,8 @@ export default function Cadastrar() {
         Buscar();
     });
 
+
+
     return (
         <div className="pagina-cadastrar">
             <div>
--- frontend/src/pages/consultar/index.jsx
+++ frontend/src/pages/consultar/index.jsx
@@ -1,88 +1,102 @@
 import { useEffect, useState } from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import { Cabe } from "../../components/cabecalho/head.jsx";
 
 import axios from "axios";
 
 export default function Consultar() {
-    const [id, setId] = useState(""); // Inicialize com uma string vazia
-    const [produtos, setProdutos] = useState([]);
+  const [id, setId] = useState(""); // Inicialize com uma string vazia
+  const [produtos, setProdutos] = useState([]);
 
-    async function buscar() {
-        const url = "http://localhost:5002/select/produto";
-        let resp = await axios.get(url);
-        setProdutos(resp.data);
-    }
+  async function buscar() {
+    const url = "http://localhost:5002/select/produto";
+    let resp = await axios.get(url);
+    setProdutos(resp.data);
+  }
 
-    async function deletar() {
-        // Remova o parâmetro id
-        const url = `http://localhost:5002/delete/produto/${id}`; // Substitua o parâmetro id
-        await axios.delete(url);
+  async function deletar() {
+    // Remova o parâmetro id
+    const url = `http://localhost:5002/delete/produto/${id}`; // Substitua o parâmetro id
+    await axios.delete(url);
 
-        //se erro
-        alert('Erro no banco de dados')
-        buscar();
-    }
+    //se erro
+    alert("Erro no banco de dados");
+    buscar();
+  }
 
-    useEffect(() => {
-         buscar();
-    }, []);
+  useEffect(() => {
+    buscar();
+  }, []);
 
-    return (
-        <div className="pagina-consultar">
+  return (
+    <div className="pagina-consultar">
+      <header>
+        <Cabe />
+      </header>
 
-            <button onClick={buscar}>Consultar</button>
-            <h1> CONSULTAR </h1>
+      <button onClick={buscar}>Consultar</button>
+      <h1> CONSULTAR </h1>
 
-            {/* <button onClick={buscar}>Buscar</button> */}
+      {/* <button onClick={buscar}>Buscar</button> */}
 
-            <div>
-                <label>Insira um id</label>
-                <input
-                    type="text"
-                    value={id}
-                    onChange={(e) => setId(e.target.value)}
-                />
-                <button onClick={deletar}>remover</button>
-            </div>
+      <div>
+        <label>Insira um id</label>
+      </div>
 
-            <div>
-                <Link to={`/cadastrar/`}>
-                    {" "}
-                    <h2>Cadastrar</h2>
+      <div>
+        <Link to={`/cadastrar/`}>
+          {" "}
+          <h2>Cadastrar</h2>
+        </Link>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Produto</th>
+            <th>Imagem</th>
+            <th>Descricao</th>
+            <th>Valor</th>
+            <th>Estoque</th>
+            <th>Disponivel</th>
+            <th className="alter">Alterar ou excluir</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {produtos.map((item) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.tipo}</td>
+              <td>{item.img}</td>
+              <td>{item.descricao}</td>
+              <td>{item.valor}</td>
+              <td>{item.estoque}</td>
+              {/* <td>{new Date(item.vinganca).toLocaleDateString()}</td> */}
+              <td>{item.disponivel ? "Sim" : "Não"}</td>
+              <td>
+                <Link to={`/cadastrar/${item.id}`}>
+                  {" "}
+                  <img src="assets\images\asf.png" alt="" width={40} />
                 </Link>
-            </div>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>tipo</th>
-                        <th>Imagem</th>
-                        <th>descricao</th>
-                        <th>Valor</th>
-                        <th>Estoque</th>
-                        <th>Disponivel</th>
-                        <th>Alterar</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {produtos.map(item => 
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.tipo}</td>
-                            <td>{item.img}</td>
-                            <td>{item.descricao}</td>
-                            <td>{item.valor}</td>
-                            <td>{item.estoque}</td>
-                            {/* <td>{new Date(item.vinganca).toLocaleDateString()}</td> */}
-                            <td>{item.disponivel ? 'Sim' : 'Não'}</td>
-                            <td><Link to ={`/cadastrar/${item.id}`}>Alterar</Link></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        </div>
-    );
+                <input
+                  type="text"
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                />
+                <Link to={`/cadastrar/${item.id}`}>
+                  <button onClick={deletar}>
+                    {" "}
+                    <img src="assets\images\asx.png" alt="" />{" "}
+                  </button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
