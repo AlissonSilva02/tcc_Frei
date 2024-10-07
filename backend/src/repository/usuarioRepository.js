e230d39bbd1f317367b9f84be2e9a5df0097daf3
alisson <ra54079082800@acaonsfatima.org.br>
2024-10-07 15:01:43 -0300

    usuarioRepository

---
+++ backend/src/repository/usuarioRepository.js
@@ -0,0 +1,29 @@
+import con from './connection.js'
+
+export async function inserirUsuario(usuario) {
+    let comando =
+        `
+    INSERT INTO usuario (nome, telefone,senha,email) VALUES
+    (?, ?, ?, ?)
+    `
+
+    let resposta = await con.query(comando, [usuario.nome, usuario.telefone, usuario.senha, usuario.email]);
+    let info = resposta[0]
+
+    return info.insertId;   
+}
+
+export async function validarUsuario(usuario) {
+    let comando = `
+       SELECT   id_autonomo  id,  
+                nome,		
+                senha
+        FROM autonomo
+        WHERE nome= ?   and senha = ?  
+     `;
+
+    let registros = await con.query(comando, [usuario.nome, usuario.senha]);
+    return registros[0][0];
+}
+
+
