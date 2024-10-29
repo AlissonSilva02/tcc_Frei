72f3b28477dd4b743fb1218229ef1a14279b9cf4
alisson <ra54079082800@acaonsfatima.org.br.com@example.com>
2024-10-29 00:02:36 -0300

    rodape quase prronto

--- frontend/src/components/rodape/index.jsx
+++ frontend/src/components/rodape/index.jsx
@@ -1,25 +1,61 @@
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 export default function Rodape() {
   return (
     <div className="secao-rodape">
+      <div className="container_superior">
+        <div className="links">
+          <Link to={"/"} style={{ textDecoration: "none" }}>
+            <h3>Inicio</h3>
+          </Link>
 
-        <div className="cor1"></div>
+          <Link to={"/produtos"} style={{ textDecoration: "none" }}>
+            <h3>Produtos</h3>
+          </Link>
 
+          <Link to={"/sobre"} style={{ textDecoration: "none" }}>
+            <h3>Sobre Nós</h3>
+          </Link>
 
-      <div className="cor">
-        <div className="linha"></div>
-        <img className="logo" src="assets\images\logo\Ashey-logo.svg" alt="logo" />
-
-        <div className="grupo">
-          <img src="assets/images/RedesSociais/face.png" alt="face" />
-
-          <img src="assets/images/RedesSociais/insta.png" alt="insta" />
-
-          <img src="assets/images/yt.png" alt="yt" />
-
+          <Link to={"/ajuda"} style={{ textDecoration: "none" }}>
+            <h3>FAQ</h3>
+          </Link>
         </div>
+
+        <Link to={"/login"}  style={{ textDecoration: "none" }}>
+          <div className="admin">
+            <h3>Admin</h3>
+          </div>
+        </Link>
+
+        <div className="cor">
+          <img
+            className="logo"
+            src="assets\images\logo\Ashey-logo.svg"
+            alt="logo"
+          />
+
+          <div className="grupo">
+            <div className="redes">
+              <img src="assets/images/RedesSociais/face.png" alt="face" />
+              <p>Ashey_Ofc</p>
+            </div>
+
+            <div className="redes">
+              <img src="assets/images/RedesSociais/insta.png" alt="insta" />
+              <p>Ashey_Ofc</p>
+            </div>
+
+            <div className="redes">
+              <img src="assets/images/yt.png" alt="yt" />
+              <p>(11)94033-0018</p>
+            </div>
+          </div>
+        </div>
+        <div className="linha"></div>
       </div>
+      <p className="copyright">© 2024 Copyright. Ashey</p>
     </div>
   );
 }
