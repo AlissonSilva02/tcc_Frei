c8397a4374bc2a937da7f3641579fafa9ddceef1
Yuri Silva <yuriteofilo38@gmail.com>
2024-10-31 23:25:49 -0300

    sobre

--- frontend/src/pages/sobrenos/index.jsx
+++ frontend/src/pages/sobrenos/index.jsx
@@ -1,4 +1,6 @@
 import Cabe from "../../components/cabecalho";
+import MenuUsuario from "../../components/MenuUsuario";
+
 import Rodape from "../../components/rodape";
 import "./index.scss";
 
@@ -7,79 +9,83 @@ export default function Sobre() {
         <div className="pagina-SobreNos">
             <div className="sobre">
                 <header>
-                    <Cabecalho_sem />
+                    <MenuUsuario />
+                    <Cabe />
                 </header>
 
-                <div className="fale">
-                    <div className="boto">
-                        <button className="butao">Fale Conosco</button>
+                <div className="faleConosco">
+                    <button className="butao">Fale Conosco</button>
+                </div>
+
+                <div className="nossaMissao">
+                    <h2>Nossa Missão</h2>
+                    <p>
+                        Acreditamos que cada pessoa é única e merece produtos
+                        que se alinhem com sua individualidade e valores. Por
+                        isso, trabalhamos para oferecer soluções eficazes que
+                        promovam sua beleza natural e promovem a autoaceitação.
+                    </p>
+                </div>
+
+                <div className="comments-txt">
+
+
+				</div>
+
+                <div className="comentarios">
+					<div className="comments-title"> 
+						<h2>Comentarios</h2>
+					</div>
+
+                    <div className="comments-txt">
+                        <img
+                            className="seta"
+                            src="assets/images/seta.esq.png"
+                            alt=""
+                        />
+                        <h3 className="pe">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Accusantium cupiditate alias quibusdam non
+                            placeat tempora, temporibus amet. Accusantium, enim
+                            consequuntur explicabo blanditiis cum exercitationem
+                            totam praesentium saepe possimus beatae. Vero.
+                        </h3>
+                        <img
+                            className="seta"
+                            src="assets/images/seta.direita.png"
+                            alt=""
+                        />
                     </div>
+                </div>
 
-                    <div className="nossa">
-                        <div className="comments-title">
-                            <h2>Nossa Missão</h2>
-                            <br />
-                            <br />
-                            <br />
-                        </div>
-                    </div>
+                <div>
+                    <p>
+                        Nossos cosméticos são formulados com ingredientes de
+                        alta qualidade, combinando inovação e respeito pelo meio
+                        ambiente. Junte-se a nós e descubra como a AuraStyle
+                        pode transformar sua rotina de cuidados pessoais.
+                    </p>
+                </div>
 
-          <div className="comments-txt">
-            <h3 className="pe">
-              Acreditamos que cada pessoa é única e merece produtos que se
-              alinhem com sua individualidade e valores. Por isso, trabalhamos
-              para oferecer soluções eficazes que promovam sua beleza natural e
-              promovem a autoaceitação.
-            </h3>
-          </div>
+                <hr />
+              
+
+                <div className="yap3">
+                    <h2>
+                        Com carinho,
+                        <br />
+                        Equipebr Aurastyle
+                    </h2>
+                </div>
+            </div>
+
+            <div>
+                <img className="tiny " src="assets/images/tiny.png" alt="" />
+            </div>
+
+            <footer>
+                <Rodape />
+            </footer>
         </div>
-
-        <div className="comentarios">
-          <h2>Comentarios</h2>
-
-          <div className="comments-txt">
-            <img className="seta" src="assets/images/seta.esq.png" alt="" />{" "}
-            <h3 className="pe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium cupiditate alias quibusdam non placeat tempora,
-              temporibus amet. Accusantium, enim consequuntur explicabo
-              blanditiis cum exercitationem totam praesentium saepe possimus
-              beatae. Vero.
-            </h3>{" "}
-            <img className="seta" src="assets/images/seta.direita.png" alt="" />
-          </div>
-        </div>
-
-                    <div>
-                        <p>
-                            Nossos cosméticos são formulados com ingredientes de
-                            alta qualidade, combinando inovação e respeito pelo
-                            meio ambiente. Junte-se a nós e descubra como a
-                            AuraStyle pode transformar sua rotina de cuidados
-                            pessoais.
-                        </p>
-                    </div>
-
-          <hr />
-          <br />
-          <br />
-
-          <div className="yap3">
-            <h2>
-              Com carinho,
-              <br />
-              Equipebr Aurastyle
-            </h2>
-          </div>
-        </div>
-      </div>
-      <div>
-        <img className="tiny " src="assets/images/tiny.png" alt="" />
-      </div>
-
-      <footer>
-        <Rodape />
-      </footer>
-    </div>
-  );
+    );
 }
