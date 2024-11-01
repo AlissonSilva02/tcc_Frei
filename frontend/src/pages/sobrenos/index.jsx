e4e0f3c9f008393cbde32249fd477373f95a14d7
alisson <ra54079082800@acaonsfatima.org.br.com@example.com>
2024-11-01 01:19:19 -0300

    filtro com where feitos

--- frontend/src/pages/sobrenos/index.jsx
+++ frontend/src/pages/sobrenos/index.jsx
@@ -3,83 +3,57 @@ import Rodape from "../../components/rodape";
 import "./index.scss";
 
 export default function Sobre() {
-    return (
-        <div className="pagina-SobreNos">
-            <div className="sobre">
-                <header>
-                    <Cabecalho_sem />
-                </header>
+  return (
+    <>
+      <div className="pagina-SobreNos">
+        <div className="sobre">
+          <header>
+            <Cabe />
+          </header>
 
-                <div className="fale">
-                    <div className="boto">
-                        <button className="butao">Fale Conosco</button>
-                    </div>
+          {/* Conteúdo da página */}
+          <div className="fale">
+            <div className="boto">
+              <button className="butao">Fale Conosco</button>
+            </div>
 
-                    <div className="nossa">
-                        <div className="comments-title">
-                            <h2>Nossa Missão</h2>
-                            <br />
-                            <br />
-                            <br />
-                        </div>
-                    </div>
+            <div className="nossa">
+              <h2>Nossa Missão</h2>
+            </div>
 
-          <div className="comments-txt">
-            <h3 className="pe">
-              Acreditamos que cada pessoa é única e merece produtos que se
-              alinhem com sua individualidade e valores. Por isso, trabalhamos
-              para oferecer soluções eficazes que promovam sua beleza natural e
-              promovem a autoaceitação.
-            </h3>
+            <div className="comments-txt">
+              <h3 className="pe">
+                Acreditamos que cada pessoa é única e merece produtos que se
+                alinhem com sua individualidade e valores...
+              </h3>
+            </div>
           </div>
-        </div>
 
-        <div className="comentarios">
-          <h2>Comentarios</h2>
-
-          <div className="comments-txt">
-            <img className="seta" src="assets/images/seta.esq.png" alt="" />{" "}
-            <h3 className="pe">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium cupiditate alias quibusdam non placeat tempora,
-              temporibus amet. Accusantium, enim consequuntur explicabo
-              blanditiis cum exercitationem totam praesentium saepe possimus
-              beatae. Vero.
-            </h3>{" "}
-            <img className="seta" src="assets/images/seta.direita.png" alt="" />
+          <div className="comentarios">
+            <h2>Comentários</h2>
+            <div className="comments-txt">
+              <img className="seta" src="assets/images/seta.esq.png" alt="" />
+              <h3 className="pe">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit...
+              </h3>
+              <img className="seta" src="assets/images/seta.direita.png" alt="" />
+            </div>
           </div>
-        </div>
 
-                    <div>
-                        <p>
-                            Nossos cosméticos são formulados com ingredientes de
-                            alta qualidade, combinando inovação e respeito pelo
-                            meio ambiente. Junte-se a nós e descubra como a
-                            AuraStyle pode transformar sua rotina de cuidados
-                            pessoais.
-                        </p>
-                    </div>
-
-          <hr />
-          <br />
-          <br />
+          <p>
+            Nossos cosméticos são formulados com ingredientes de alta qualidade...
+          </p>
 
           <div className="yap3">
-            <h2>
-              Com carinho,
-              <br />
-              Equipebr Aurastyle
-            </h2>
+            <h2>Com carinho,<br />Equipe Aurastyle</h2>
           </div>
         </div>
       </div>
-      <div>
-        <img className="tiny " src="assets/images/tiny.png" alt="" />
-      </div>
 
+      <img className="tiny" src="/assets/images/tiny.png" alt="" />
       <footer>
         <Rodape />
       </footer>
-    </div>
+    </>
   );
 }
