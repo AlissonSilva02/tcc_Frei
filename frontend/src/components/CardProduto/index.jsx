8a1cb3b26dfd55bac83fe725e3bb8d4344ec5dac
Yuri Silva <yuriteofilo38@gmail.com>
2024-11-03 15:57:16 -0300

    Linkando Produtos

--- frontend/src/components/CardProduto/index.jsx
+++ frontend/src/components/CardProduto/index.jsx
@@ -1,19 +1,17 @@
 import './index.scss';
-import { Link } from "react-router-dom"
 
-
-export default function Card({ imagem, alt, preco, nome }) {
+export default function Card({ imagem, alt, preco, nome, link }) {
     return (
 
         <div className='card-component'>
 
-            <Link>
+            <div onClick={() => window.location.href = `/produto/${link}`} >
                 <div>
                     <img src={imagem} alt={alt} />
                         <h2>{nome}</h2>
                     <h3>R$  {Number(preco).toFixed(2)}</h3>
                 </div>
-            </Link>
+            </div>
         </div>
 
     )
--- frontend/src/pages/Produto/index.jsx
+++ frontend/src/pages/Produto/index.jsx
@@ -69,7 +69,7 @@ export default function Produto() {
 				<button onClick={() => alert(JSON.stringify(produto.categoria))}>produto</button>
 
 				<div className="botao-voltar">
-					<Link to={"/"}>
+					<Link to={-1}>
 						<div className="voltar">
 							<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
 								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" />
@@ -137,6 +137,7 @@ export default function Produto() {
 								alt={item.img}
 								preco={item.valor}
 								nome={item.nome}
+								link={item.id}
 							/>
 						))}
 
