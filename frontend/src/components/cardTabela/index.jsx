ce941a8f7ab15df322caae46a0023f6b3e8dfc69
alisson <ra54079082800@acaonsfatima.org.br.com@example.com>
2024-11-07 23:54:56 -0300

    card consultar

--- frontend/src/components/cardTabela/index.jsx
+++ frontend/src/components/cardTabela/index.jsx
@@ -6,89 +6,104 @@ import { MagicMotion } from "react-magic-motion";
 
 import axios from "axios";
 
-
 export default function CardTabela({ token }) {
-    const [limite, setLimite] = useState(5);
-    const [produtos, setProdutos] = useState([]);
-    const [mostrarVermais, setMostrarVermais] = useState(true);
+  const [limite, setLimite] = useState(5);
+  const [produtos, setProdutos] = useState([]);
+  const [mostrarVermais, setMostrarVermais] = useState(true);
 
-    const buscar = useCallback(
-        async (token) => {
-            const url = `http://${host}/select/produto/?total=${limite}&x-access-token=${token}`;
-            let resp = await axios.get(url);
+  const buscar = useCallback(
+    async (token) => {
+      const url = `http://${host}/select/produto/?total=${limite}&x-access-token=${token}`;
+      let resp = await axios.get(url);
 
-            const itens = Array.isArray(resp.data) ? resp.data : [];
+      const itens = Array.isArray(resp.data) ? resp.data : [];
 
-            setProdutos(itens);
-        },
-        [limite]
-    );
+      setProdutos(itens);
+    },
+    [limite]
+  );
 
-
-    async function VerMais() {
-        if (limite !== produtos.length) {
-            setMostrarVermais(false);
-        }
-
-        let novoLimite = limite * 2;
-        setLimite(novoLimite);
-
-        await buscar(token);
+  async function VerMais() {
+    if (limite !== produtos.length) {
+      setMostrarVermais(false);
     }
 
-    const host = "localhost:5031";
+    let novoLimite = limite * 2;
+    setLimite(novoLimite);
 
-    async function deletar(id, token) {
-        // Remova o parâmetro id
-        const url = `http://${host}/delete/produto/${id}?x-access-token=${token}`; // Substitua o parâmetro id
-        await axios.delete(url);
+    await buscar(token);
+  }
 
-        await buscar(token);
-    }
+  const host = "localhost:5031";
 
-    useEffect(() => {
-        buscar(token);
-    }, [buscar]);
+  async function deletar(id, token) {
+    // Remova o parâmetro id
+    const url = `http://${host}/delete/produto/${id}?x-access-token=${token}`; // Substitua o parâmetro id
+    await axios.delete(url);
 
+    await buscar(token);
+  }
 
-    return (
-        <div className='CardTabela'>
+  useEffect(() => {
+    buscar(token);
+  }, [buscar]);
 
-            <div>
-                <img src={produtos.imagem} alt={produtos.alt} />
-                <p>id: {produtos.id}</p>
-            </div>
+  return (
+  <MagicMotion>
+    <div className="CardTabela">
+      <div className="componente-TabelaLista">
+        {produtos.length > 0 ? (
+          <tbody>
+            {produtos.map((item, index) => (
+              <tr key={index}>
+                <div className="imagem">
+                  <img src={item.img} alt="" />
+                  <p>Id: {item.id.toString().padStart(2, "0")}</p>
+                </div>
 
-            <div>
-                <h4>{produtos.nome}</h4>
-                <p>Estoque: {produtos.estoque}</p>
-                <h5>Preço: {produtos.preco}</h5>
-            </div>
-
-            <div>
-                <MagicMotion>
-                    <Link to={`/cadastrar/${produtos.id}`}>
-                        <img
-                            src="/assets/images/editar.png"
-                            alt="icone_alterar"
-                            width={48}
-                        />
+                <div className="especificacoes">
+                  <h3>{item.nome}</h3>
+                  <div className="valores">
+                    <h4>Estoque: {item.estoque}</h4>
+                    <h4>R$ {item.valor}</h4>
+                  </div>
+                </div>
+                <div className="alteracoes">
+                  
+                    <Link to={`/cadastrar/${item.id}`}>
+                      <img
+                        src="/assets/images/editar.png"
+                        alt="icone_alterar"
+                        width={48}
+                      />
                     </Link>
 
-                    <Link
-                        onClick={() =>
-                            deletar(produtos.id, token)
-                        }
-                    >
-                        <img
-                            src="/assets/images/Remover.svg"
-                            alt="icone_lixo"
-                            width={48}
-                        />
+                    <Link onClick={() => deletar(item.id, token)}>
+                      <img
+                        src="/assets/images/Remover.svg"
+                        alt="icone_lixo"
+                        width={48}
+                      />
                     </Link>
-                </MagicMotion>
-            </div>
+                  
+                </div>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <div className="nenhum">
+            <p>Nenhum produto encontrado</p>
+          </div>
+        )}
+      </div>
 
+      {mostrarVermais && (
+        <div className="verMais">
+          <hr />
+          <button onClick={VerMais}>Ver Mais</button>
+          <hr />
         </div>
-    )
+      )}
+    </div>
+</MagicMotion>  );
 }
